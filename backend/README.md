4fc0758e1975d6e12c8065c808d8639e4e6e171d
farvell <farvellsmurf@gmail.com>
2025-02-24 22:07:13 +0100

    Deleted backend folder

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# followers-overlay
-Uses Twitch API to get the list of the 30 latest followers, and use it as an overlay for obs.
--- backend/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import handleCallback from '../controllers/auth.controller.js';
-
-const router = Router();
-
-// Ruta de autenticación
-router.get('/callback', handleCallback);
-
-export default router;
